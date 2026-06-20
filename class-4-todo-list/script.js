c7c3eccda8106380c702a8d8b029cd678cdcd33a
umaimayaseen <umaimakh0609@gmail.com>
2026-06-21 02:19:08 +0500

    adding classes

---
+++ class-4-todo-list/script.js
@@ -0,0 +1,46 @@
+let todoList = [];
+
+function addingTask() {
+    let inputField = document.getElementById("task-input");
+    let newTask = inputField.value;
+
+    if (newTask === "" || newTask === "  ") {
+        alert("Please Enter a Task!!");
+        return;
+    }
+
+    todoList.push(newTask);
+    inputField.value = "";
+    showList()
+}
+
+function showList() {
+    let deliveryBox = document.getElementById("list-container");
+    let finalHTML = "";
+    for (let i = 0; i < todoList.length; i++) {
+        finalHTML = finalHTML + `
+        <p> ${todoList[i]}
+            <button onclick= "editTask(${i})" >Edit</button>
+           <button onclick="deleteTask(${i})" style=" background-color:red;">Delete</button>
+          
+        </p> `
+
+    }
+    deliveryBox.innerHTML = finalHTML;
+}
+
+function deleteTask(index) {
+    todoList.splice(index, 1);
+    showList()
+
+}
+
+function editTask(index) {
+    let updatedTask = prompt("Edit Your Task!", todoList[index]);
+
+    if (updatedTask !== null && updatedTask !== "") {
+        todoList[index] = updatedTask;
+        showList();
+    }
+
+}
